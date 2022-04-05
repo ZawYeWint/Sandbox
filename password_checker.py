002619d42b3856801a28fe416602e75d07eb0022
Zaw Ye Wint <zawyewint@gusto.edu.mm>
2022-04-06 00:05:28 +0800

    Modify function to add criteria for valid password

--- password_checker.py
+++ password_checker.py
@@ -6,11 +6,11 @@ Name: Patrick
 
 def is_valid_password(text):
     """Check whether a given text has correct password format"""
-    return 8 <= len(text) <= 20
+    return 8 <= len(text) <= 20 and "#" in text
 
 
 def main():
-    new_password = "helloworld"
+    new_password = "helloworld#"
     print(f"{new_password} is a valid password? {is_valid_password(new_password)}")
 
 
